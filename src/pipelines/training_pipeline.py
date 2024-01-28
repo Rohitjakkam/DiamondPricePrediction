f798a7c436dc7b8eba624e10eb2c22a6621f727a
Rohitjakkam <rohitjakkam@gmail.com>
2024-01-28 17:01:48 +0530

    Model Trainer

--- src/pipelines/training_pipeline.py
+++ src/pipelines/training_pipeline.py
@@ -6,6 +6,7 @@ import pandas as pd
 
 from src.components.data_ingestion import DataIngestion
 from src.components.data_transformation import DataTransformation
+from src.components.model_trainer import ModelTrainer
 
 if __name__ =='__main__':
     obj=DataIngestion()
@@ -14,3 +15,7 @@ if __name__ =='__main__':
 
     data_transfromation = DataTransformation()
     train_arr, test_arr, obj_path = data_transfromation.initiate_data_tranformation(train_data_path,test_data_path)
+
+
+    model_trainer = ModelTrainer()
+    model_trainer.initiate_model_training(train_arr, test_arr)
--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,8 @@ from src.exception import CustomException
 import numpy as np 
 import pandas as pd 
 
+from sklearn.metrics import r2_score,mean_absolute_error,mean_squared_error
+
 def save_object(file_path,obj):
     try:
         dir_path = os.path.dirname(file_path)
@@ -14,3 +16,28 @@ def save_object(file_path,obj):
             pickle.dump(obj, file_obj)
     except Exception as e:
         raise CustomException(e,sys)
+    
+def evaluate_model(X_train, y_train, X_test, y_test, models):
+    try:
+        report = {}
+        for i in range(len(models)):
+            model = list(models.values())[i]
+            # Train model
+            model.fit(X_train,y_train)
+
+            # Predict Testing Data
+            y_test_pred = model.predict(X_test)
+
+
+            # Get R2 scores for train and test data
+
+            test_model_score = r2_score(y_test,y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
+        return report
+
+
+    except Exception as e:
+        logging.info('Expection occured during model training')
+        raise Exception(e,sys)
